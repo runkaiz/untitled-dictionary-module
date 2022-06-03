d3b6c6fcc3b56978d17eea7c3009740fb4e4614f
Runkai Zhang <me@runkaizhang.xyz>
2022-06-03 17:32:56 -0400

    Add tailwindcss

--- src/lib/components/Dictionary.svelte
+++ src/lib/components/Dictionary.svelte
@@ -1,4 +1,5 @@
 <script>
+    import '../tailwind.css';
     export let key = "";
 
     async function lookup() {
---
+++ tailwind.config.cjs
@@ -0,0 +1,28 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+
+module.exports = {
+	content: ['./src/**/*.{svelte, css}', './src/app.html'],
+	media: false,
+	theme: {
+		fontFamily: {
+			sans: ['Inter', ...defaultTheme.fontFamily.sans]
+		},
+		extend: {
+			colors: {
+				BlueNCS: {
+					100: '#EDF7FC',
+					200: '#DCEEF8',
+					300: '#E4F1F9',
+					400: '#D3E7F1',
+					500: '#89BCD7',
+					600: '#056EA6'
+				}
+			},
+			order: {
+				second: '-9998'
+			}
+		}
+	},
+	variants: {},
+	plugins: [require('@tailwindcss/forms')]
+};
